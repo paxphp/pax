c3f338dec1f9094a970b93d85c672c9408ea4006
Nick <nick.dietel@gmail.com>
2012-08-15 01:13:10 +0200

    implemented outsourced actions and added new ones

--- Response.php
+++ Response.php
@@ -3,17 +3,41 @@ namespace dsx\PAX;
 
 class Response extends Responder{
 	
+	public function bind($sDestination, $sEvent, $sFunction){
+		$this->addTask(['d'=>$sDestination, 'e'=>$sEvent,'f'=>$sFunction]);
+		return $this;
+	}
+	public function unbind($sDestination, $sEvent){
+		$this->addTask(['d'=>$sDestination, 'e'=>$sEvent]);
+		return $this;
+	}
 	public function html($sDestination, $sContent){
 		$this->addTask(['d'=>$sDestination, 'c'=>$sContent], 'html');
+		return $this;
 	}
 	public function append($sDestination, $sContent){
 		$this->addTask(['d'=>$sDestination, 'c'=>$sContent]);
+		return $this;
+	}
+	public function attr($sDestination, $sAttribute, $sContent){
+		$this->addTask(['d'=>$sDestination, 'a'=>$sAttribute, 'c'=>$sContent]);
+		return $this;
 	}
 	public function prepend($sDestination, $sContent){
 		$this->addTask(['d'=>$sDestination, 'c'=>$sContent]);
+		return $this;
 	}
 	public function console($sContent){
 		$this->addTask();
+		return $this;
+	}
+	public function alert($sContent){
+		$this->addTask();
+		return $this;
+	}
+	public function script($sContent){
+		$this->addTask(['c'=>$sContent]);
+		return $this;
 	}
 	
 }
