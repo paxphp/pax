6330bf3c1e7e67dae4071e166a20963952dc4f8e
Nick <nick.dietel@gmail.com>
2012-08-16 00:57:28 +0200

    included actions into PAX to keep it simple

--- jquery.PAX.js
+++ jquery.PAX.js
@@ -50,6 +50,15 @@
 	};
 	
 	var builtinActions = {
+			bind 	: function(oJob){	jQuery.globalEval( "$('" + oJob.d + "').bind('" + oJob.e + "', function(){" + oJob.f + ";})" );},
+			unbind	: function(oJob){	$(oJob.d).unbind(oJob.e);},
+			html 	: function(oJob){	$(oJob.d).html(oJob.c);},
+			append 	: function(oJob){$(oJob.d).append(oJob.c);},
+			attr 	: function(oJob){$(oJob.d).attr(oJob.a,oJob.c);},
+			prepend	: function(oJob){$(oJob.d).prepend(oJob.c);},
+			console	: function(oJob){if(typeof console =="object")console.log(oJob[0]);},
+			alert	: function(oJob){alert(oJob[0]);},
+			script	: function(oJob){$(document).ready(function() {jQuery.globalEval(oJob.c);});}
 	};
 	
 	var defaultData = {
