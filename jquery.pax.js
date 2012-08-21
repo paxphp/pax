9e58523ed7905c45e347a5fd0672d46639734aa7
Nick <nick.dietel@gmail.com>
2012-08-21 21:59:15 +0200

    renamed jquery.PAX

--- jquery.pax.js
+++ jquery.pax.js
@@ -29,10 +29,10 @@
 	};
 	
 	var setData = function(data){
-		return $(document).data('PAX',data);
+		return $(document).data('pax',data);
 	};
 	var getData = function(){
-		return $(document).data('PAX');
+		return $(document).data('pax');
 	};
 	var hasData = function(){
 		if(typeof getData() != 'object'){
@@ -50,15 +50,22 @@
 	};
 	
 	var builtinActions = {
-			bind 	: function(oJob){	jQuery.globalEval( "$('" + oJob.d + "').bind('" + oJob.e + "', function(){" + oJob.f + ";})" );},
-			unbind	: function(oJob){	$(oJob.d).unbind(oJob.e);},
-			html 	: function(oJob){	$(oJob.d).html(oJob.c);},
-			append 	: function(oJob){$(oJob.d).append(oJob.c);},
-			attr 	: function(oJob){$(oJob.d).attr(oJob.a,oJob.c);},
-			prepend	: function(oJob){$(oJob.d).prepend(oJob.c);},
-			console	: function(oJob){if(typeof console =="object")console.log(oJob[0]);},
-			alert	: function(oJob){alert(oJob[0]);},
-			script	: function(oJob){$(document).ready(function() {jQuery.globalEval(oJob.c);});}
+			bind 		: function(oJob){	jQuery.globalEval( "$('" + oJob.d + "').bind('" + oJob.e + "', function(){" + oJob.f + ";})" );},
+			unbind		: function(oJob){	$(oJob.d).unbind(oJob.e);},
+			html 		: function(oJob){	$(oJob.d).html(oJob.c);},
+			append 		: function(oJob){	$(oJob.d).append(oJob.c);	},
+			prepend		: function(oJob){	$(oJob.d).prepend(oJob.c);	},
+			before		: function(oJob){	$(oJob.d).before(oJob.c);	},
+			after		: function(oJob){	$(oJob.d).after(oJob.c);	},
+			attr 		: function(oJob){	$(oJob.d).attr(oJob.a,oJob.c);	},
+			console		: function(oJob){	if(typeof console =="object")console.log(oJob[0]);	},
+			alert		: function(oJob){	alert(oJob[0]);	},
+			script		: function(oJob){	$(document).ready(function() {jQuery.globalEval(oJob.c);});	},
+			css			: function(oJob){	$(oJob.d).css(oJob.p, oJob.v);	},
+			addClass	: function(oJob){	$(oJob.d).addClass(oJob.c);	},
+			removeClass	: function(oJob){	$(oJob.d).removeClass((typeof oJob.c === "string")?oJob.c:undefined);	},
+			_debug		: function(oJob){	console.log(oJob);	}
+			
 	};
 	
 	var defaultData = {
@@ -80,14 +87,14 @@
 		}
 	};
 	
-	$.PAX = function( method ){
+	$.pax = function( method ){
 		if(methods[method]) {
 			method = methods[method];
 			arguments = Array.prototype.slice.call(arguments, 1);
 		} else if( typeof(method) == 'object' || !method ) {
 			method = methods.init;
 		} else {
-			$.error( 'Method ' +  method + ' does not exist on jQuery.PAX' );
+			$.error( 'Method ' +  method + ' does not exist on jQuery.pax' );
 			return this;
 		}
  
@@ -96,4 +103,4 @@
 	
 })( jQuery );
 
-$.PAX();
+$.pax();
