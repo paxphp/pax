cf3ad0e32d522b7ee720b2c03c9c1bee7aa93858
nickdietel <nick.dietel@gmail.com>
2015-05-21 18:01:58 +0200

    enabled chaining again

--- Response.php
+++ Response.php
@@ -2,70 +2,92 @@
 namespace Pax;
 
 use Pax\Tasks as t;
+use Pax\Tasks\AbstractTask;
 
 class Response extends Responder{
 	
 	public function bind($sDestination, $sEvent, $sFunction){
-		return $this->addTask(['d'=>$sDestination, 'e'=>$sEvent,'f'=>$sFunction]);
+		$this->addTask(['d'=>$sDestination, 'e'=>$sEvent,'f'=>$sFunction]);
+		return $this;
 	}
 	public function unbind($sDestination, $sEvent){
-		return $this->addTask(['d'=>$sDestination, 'e'=>$sEvent]);
+		$this->addTask(['d'=>$sDestination, 'e'=>$sEvent]);
+		return $this;
 	}
 	public function html($sDestination, $sContent){
-		return $this->addTask(['d'=>$sDestination, 'c'=>$sContent], 'html');
+		$this->addTask(['d'=>$sDestination, 'c'=>$sContent], 'html');
+		return $this;
 	}
 	public function append($sDestination, $sContent){
-		return $this->addTask(['d'=>$sDestination, 'c'=>$sContent]);
+		$this->addTask(['d'=>$sDestination, 'c'=>$sContent]);
+		return $this;
 	}
 	public function prepend($sDestination, $sContent){
-		return $this->addTask(['d'=>$sDestination, 'c'=>$sContent]);
+		$this->addTask(['d'=>$sDestination, 'c'=>$sContent]);
+		return $this;
 	}
 	public function before($sDestination, $sContent){
-		return $this->addTask(['d'=>$sDestination, 'c'=>$sContent]);
+		$this->addTask(['d'=>$sDestination, 'c'=>$sContent]);
+		return $this;
 	}
 	public function after($sDestination, $sContent){
-		return $this->addTask(['d'=>$sDestination, 'c'=>$sContent]);
+		$this->addTask(['d'=>$sDestination, 'c'=>$sContent]);
+		return $this;
 	}
 	public function attr($sDestination, $sAttribute, $sContent){
-		return $this->addTask(t\AttrTask::create($sDestination, $sAttribute, $sContent));
+		$this->addTask(t\AttrTask::create($sDestination, $sAttribute, $sContent));
+		return $this;
 	}
 	public function console($sContent){
-		return $this->addTask();
+		$this->addTask();
+		return $this;
 	}
 	public function alert($sContent){
-	    return $this->addTask(t\AlertTask::create($sContent));
+	    $this->addTask(t\AlertTask::create($sContent));
+	    return $this;
 	}
 	public function script($sContent){
-		return $this->addTask(['c'=>$sContent]);
+		$this->addTask(['c'=>$sContent]);
+		return $this;
 	}
 	public function _debug($args){
-		return $this->addTask();
+		$this->addTask();
+		return $this;
 	}
 	public function css($sDestination, $sPropertyName, $sValue){
-		return $this->addTask(['d'=>$sDestination, 'p'=>$sPropertyName, 'v'=>$sValue]);
+		$this->addTask(['d'=>$sDestination, 'p'=>$sPropertyName, 'v'=>$sValue]);
+		return $this;
 	}
 	public function addClass($sDestination, $sClassName){
-		return $this->addTask(['d'=>$sDestination, 'c'=>$sClassName]);
+		$this->addTask(['d'=>$sDestination, 'c'=>$sClassName]);
+		return $this;
 	}
 	public function removeClass($sDestination, $sClassName=null){
-		return $this->addTask(['d'=>$sDestination, 'c'=>$sClassName]);
+		$this->addTask(['d'=>$sDestination, 'c'=>$sClassName]);
+		return $this;
 	}
 	public function remove($sDestination){
-	    return $this->addTask(['d'=>$sDestination]);
+	    $this->addTask(['d'=>$sDestination]);
+	    return $this;
 	}
 	public function toggle($sDestination){
-	    return $this->addTask(['d'=>$sDestination]);
+	    $this->addTask(['d'=>$sDestination]);
+	    return $this;
 	}
 	public function show($sDestination){
-	    return $this->addTask(['d'=>$sDestination]);
+	    $this->addTask(['d'=>$sDestination]);
+	    return $this;
 	}
 	public function hide($sDestination){
-	    return $this->addTask(['d'=>$sDestination]);
+	    $this->addTask(['d'=>$sDestination]);
+	    return $this;
 	}
 	public function replaceWith($sDestination, $sContent=null){
-	    return $this->addTask(['d'=>$sDestination, 'c'=>$sContent]);
+	    $this->addTask(['d'=>$sDestination, 'c'=>$sContent]);
+	    return $this;
 	}
 	public function when($condition, AbstractTask $true, AbstractTask $false = NULL){
 	    $this->addTask(t\WhenTask::create($condition, $true, $false));
+	    return $this;
 	}
 }
